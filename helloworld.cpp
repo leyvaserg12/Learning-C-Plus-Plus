45e784d65e6aaf5900d7eca3764235cb6c704e7f
Serg Leyva <leyvaserg12@gmail.com>
2025-11-15 19:17:13 -0600

    learned basic cpp character printing (std::cout and std::endl)

--- helloworld.cpp
+++ helloworld.cpp
@@ -1,6 +1,8 @@
-#include <iostream>
+#include <iostream> /* iostream -> header file for basic input and output operations */
 
 int main(){
-    std::cout << "Hello World!"; /* standard character out */
+    std::cout << "Hello World!" << std::endl; /* std::cout -> standard character out, std::endl -> end line (ie. "\n")*/
+
+    std::cout << "My Name is Serg" << '\n'; /*std:endl and '\n' basically the same to end line ('\n' better performance wise)*/
     return 0;
 }
